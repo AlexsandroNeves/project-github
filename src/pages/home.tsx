e81401c1ff1a1fd2f873a07eab05f9712b62c76f
Alexsandro Neves <alexsandrodnss@gmail.com>
2023-02-17 13:35:45 -0300

    ajustando codigo

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,22 +1,28 @@
 import { useState } from "react"
 import { Users } from "../interface/user"
 import { Search } from "../components/search/Search"
-import api from 'axios'
+import axios from 'axios'
 import { User } from "../components/user/Index"
 import "../components/user/style.css"
 import { Error } from "./style"
 const Home = () => {
 
     const [user, setUser] = useState<Users>()
-    const [inputError, setInputError] = useState(false);
+    const [inputError, setInputError] = useState('');
 
     const loadUser = async (userName: string) => {
+
+        if (!userName) {
+            setInputError('Digite nome de usuário');
+            return;
+          }
+    
         try {
-            setInputError(false)
-            await api.get(`users/${userName}`)
-                .then((res) => {
-                    setUser(res.data)
-                })
+            await axios.get(`https://api.github.com/users/${userName}`)
+            .then((res) => {
+                setUser(res.data)
+            })
+            setInputError('')
         }
         catch (error) {
             setInputError("Usuario não encontrado");
