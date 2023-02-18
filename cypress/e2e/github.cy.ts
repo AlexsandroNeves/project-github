41d6f6916644038217102b5220100db760409403
Alexsandro Neves <alexsandrodnss@gmail.com>
2023-02-18 00:13:29 -0300

    subindo cypress

--- cypress/e2e/github.cy.ts
+++ cypress/e2e/github.cy.ts
@@ -1,13 +1,14 @@
-
-
-
 describe('Teste do ', () => {
   it('Testando o componente', () => {
     cy.visit('https://project-github-alexsandroneves.vercel.app/')
+    cy.wait(2000) 
     cy.get('button').click()
     cy.get('.sc-hLBbgP').type('omariosouto')
+    cy.wait(2000) 
     cy.get('button').click()
+    cy.wait(2000) 
     cy.get('.kYsYdw').click()
+    cy.wait(2000) 
     cy.get('.MuiButtonBase-root').click()
 
   
